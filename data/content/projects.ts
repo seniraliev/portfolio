56d4633d3c81dd578baa1667a1476c739f39229d
seniraliev <seniraliev94@gmail.com>
2023-04-13 07:48:33 -0400

    modify projects list

--- data/content/projects.ts
+++ data/content/projects.ts
@@ -2,16 +2,16 @@ import { kebabCase, kebabArray } from "@/utils/utils";
 import { Project } from "types";
 
 const projects: Project[] = [
+  // {
+  //   id: 0,
+  //   title: "Yearn Classic Finance",
+  //   desc: "Yearn Classic Finance platform of the DeFi and NFT ecosystem on Binance Smart Chain",
+  //   img: "/static/projects/yearnclassic.png",
+  //   link: "https://earnx.yearnclassic.finance/",
+  //   tags: ["Next", "React", "Redux", "SCSS", "Solidity", "Remix", "Web3.js"],
+  // },
   {
     id: 0,
-    title: "Yearn Classic Finance",
-    desc: "Yearn Classic Finance platform of the DeFi and NFT ecosystem on Binance Smart Chain",
-    img: "/static/projects/yearnclassic.png",
-    link: "https://earnx.yearnclassic.finance/",
-    tags: ["Next", "React", "Redux", "SCSS", "Solidity", "Remix", "Web3.js"],
-  },
-  {
-    id: 1,
     title: "Treedefi",
     desc: "Eco-friendly platform with DEX, Pools, and Marketplace on Binance Smart Chain",
     img: "/static/projects/treedefi.png",
@@ -19,7 +19,7 @@ const projects: Project[] = [
     tags: ["React", "MobX", "GSAP", "SCSS", "Material UI", "Solidity", "Remix", "Ethers.js"],
   },
   {
-    id: 2,
+    id: 1,
     title: "Lucky Zeros",
     desc: "Lucky Zero NFT collection of 8,888 items on Ethereum network",
     img: "/static/projects/luckyzeros.png",
@@ -27,7 +27,7 @@ const projects: Project[] = [
     tags: ["Next", "React", "CSS", "Solidity", "Hardhat", "Web3.js"],
   },
   {
-    id: 3,
+    id: 2,
     title: "Blockchain Games",
     desc: "NFT staking, Raffle, Slots, Coinflip game on Solana network",
     img: "/static/projects/solkitties.png",
@@ -35,7 +35,7 @@ const projects: Project[] = [
     tags: ["Rust", "Solana/Web3.js", "Anchor"],
   },
   {
-    id: 4,
+    id: 3,
     title: "Deez kits games",
     desc: "Slots and Coinflip game for Deez kits on Solana network",
     img: "/static/projects/deezkits.png",
@@ -43,7 +43,7 @@ const projects: Project[] = [
     tags: ["React", "Redux", "SASS", "Material UI", "Solana/Web3.js", "Rust", "Anchor"],
   },
   {
-    id: 5,
+    id: 4,
     title: "SpookySwap",
     desc: "SpookySwap DeFi protocol on Fantom Opera",
     img: "/static/projects/spookyswap.png",
